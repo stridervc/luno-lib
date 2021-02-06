e3ca175f48e85269e622ad3f6f886c77e6a66670
Fred Strauss <fred@strider.co.za>
2021-02-06 08:58:55 +0200

    Get basic ticker query working

--- src/Luno.hs
+++ src/Luno.hs
@@ -4,18 +4,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Luno
-  (
+  ( lunoClient
+  , ticker
+  , tickers
   ) where
 
-import Control.Applicative
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Except
 import Data.Aeson
-import Data.Monoid
 import Data.Proxy
-import Data.Text (Text)
 import GHC.Generics
+import Network.HTTP.Client (newManager, defaultManagerSettings)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Servant.API
 import Servant.Client
 
@@ -23,15 +21,26 @@ import qualified Data.Text    as T
 import qualified Data.Text.IO as T
 
 -- |API endpoints
-type LunoAPI =
-  "tickers" :> Get '[JSON] [Ticker]
+type LunoAPI = "ticker"  :> QueryParam "pair" T.Text :> Get '[JSON] Ticker
+          :<|> "tickers" :> Get '[JSON] [Ticker]
 
-type Price  = Text
-type Pair   = Text
-type Amount = Text
-type Status = Text
+type Price  = Float
+type Pair   = T.Text
+type Amount = Float
 
--- |Ticker for a single pair
+data Status = ACTIVE | POSTONLY | DISABLED deriving (Eq, Show, Read)
+
+-- Luno uses strings instead of numbers in most cases
+readPrice :: T.Text -> Price
+readPrice = read . T.unpack
+
+readAmount :: T.Text -> Amount
+readAmount = read . T.unpack
+
+readStatus :: T.Text -> Status
+readStatus = read . T.unpack
+
+-- | Ticker for a single currency pair
 data Ticker = Ticker
   { ask                 :: Price
   , bid                 :: Price
@@ -39,23 +48,27 @@ data Ticker = Ticker
   , pair                :: Pair
   , rolling24HourVolume :: Amount
   , status              :: Status
-  , timestamp           :: Text
+  , timestamp           :: Integer
   } deriving (Eq, Show)
 
 instance FromJSON Ticker where
   parseJSON (Object o) =
-    Ticker  <$> o .: "ask"
-            <*> o .: "bid"
-            <*> o .: "last_trade"
+    Ticker  <$> (readPrice <$> o .: "ask")
+            <*> (readPrice <$> o .: "bid")
+            <*> (readPrice <$> o .: "last_trade")
             <*> o .: "pair"
-            <*> o .: "rolling_24_hour_volume"
-            <*> o .: "status"
+            <*> (readAmount <$> o .: "rolling_24_hour_volume")
+            <*> (readStatus <$> o .: "status")
             <*> o .: "timestamp"
-
-  parseJSON _ = mzero
 
 lunoAPI :: Proxy LunoAPI
 lunoAPI = Proxy
 
--- getTickers :: ExceptT ServantError IO [Ticker]
-getTickers = client lunoAPI -- (BaseUrl Https "https://api.luno.com/api/1/" 443)
+ticker :: Maybe Pair -> ClientM Ticker
+tickers :: ClientM [Ticker]
+
+ticker :<|> tickers = client lunoAPI
+
+lunoClient = do
+  manager <- newManager tlsManagerSettings
+  return $ mkClientEnv manager (BaseUrl Https "api.luno.com" 443 "api/1")
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Luno
+import Servant.Client
+import qualified Data.Text as T
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  client <- lunoClient
+  res <- runClientM (ticker (Just "XBTZAR")) client
+
+  putStrLn ""
+  print res
